6e76b80f08478194fb855e53702df0b7fbc54a80
Naresh <t.naresh5422@gmail.com>
2022-08-02 14:25:56 +0530

    Adding new file into git repository

---
+++ Test.R
@@ -0,0 +1,133 @@
+print("Hello Naresh")
+x <-  18
+y <- 4
+z <- x+y
+print(z)
+z
+18 + 4
+18 - 4
+18 * 4
+18 / 2
+18.5/3
+2 ** 3
+2 ^ 3
+4.4 ^ 2
+9 ** 0.5
+9 ** .5
+9 ** 1/2
+9 ** (1/2)
+
+
+14 %% 4
+14.75 %% 4
+10 %% 3.5
+8 / 3
+8 %/% 3
+-8 %/% 3
+-8 / 3
+8 %/% -3
+TRUE + TRUE
+FALSE - FALSE
+TRUE + FALSE
+FALSE + TRUE
+
+
+'Hello' + 5
+'Hello' + 'hi'
+
+
+
+
+
+x = 10
+typeof(x)
+mode(x)
+storage.mode(x)
+
+age <- 28
+marks1 <- 100
+sub.java <- 29
+sub.java
+sub_j = 12
+sub_j
+
+
+
+
+
+
+
+
+
+
+
+
+x = 10
+typeof(x)
+mode(x)
+storage.mode(x)
+class(x)
+x = 10.5
+x = 'hELLO'
+typeof(x)
+class(x)
+mode(x)
+storage.mode((x))
+b = 20
+
+r = a+b
+r
+
+# Predefined Keywords
+
+if
+else
+nullfile()
+break <- 10
+for
+
+
+# data Types
+x <- 10.24
+y = 10L
+x
+class(x)
+
+is.integer(x)
+ 
+x = as.integer(10L)
+
+class(x)
+
+traceback()
+
+
+
+
+# Indicate whether the numbver is palindrome or not
+palindrome <- function(num){
+  dig <- floor(log(num, 10))
+  for (x in 0:((dig %?% 2) + 1)){
+    place1 <- dig - x
+  }
+  
+}
+
+
+best <- 0
+for(x in 100:999){
+  for(y in x:999){
+    candi <- x*y
+    if(candi > best && palindrome(candi)){
+      best <- candi
+    }
+  }
+}
+
+browser()
+
+
+
+
+
+debugonce(devtools::install)
